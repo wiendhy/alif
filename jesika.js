a3b78450e3ecfa5e3ee590a7ed4407a39db7516f
wiendhy <43066026+wiendhy@users.noreply.github.com>
2018-10-13 20:38:27 +0700

    Add files via upload

---
+++ jesika.js
@@ -0,0 +1,4 @@
+<!-- SCM Music Player http://scmplayer.co -->
+<script type="text/javascript" src="http://scmplayer.co/script.js" 
+data-config="{'skin':'skins/tunes/skin.css','volume':50,'autoplay':true,'shuffle':true,'repeat':1,'placement':'bottom','showplaylist':true,'playlist':[{'title':'05 Jessica Kill','url':'https://sites.google.com/site/theboegis2/about-me/theboegis/05%20Jessica%20Kill.mp3'}]}" ></script>
+<!-- SCM Music Player script end -->
